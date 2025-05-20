63aba6dea032616ee3b1429286a072d9286d52c9
unknown <intesedzel@gmail.com>
2025-05-21 00:08:39 +0800

    Setup: App icon, fonts, and all packages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,33 @@
+import { useColorScheme } from '@/hooks/useColorScheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { LogBox } from 'react-native';
+import { KeyboardProvider } from "react-native-keyboard-controller";
+import { configureReanimatedLogger, ReanimatedLogLevel } from 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+LogBox.ignoreAllLogs(true);
+configureReanimatedLogger({
+  level: ReanimatedLogLevel.warn,
+  strict: false,
+});
+
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const queryClient = new QueryClient();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    poppinsBlack: require('../assets/fonts/Poppins-Black.ttf'),
+    poppinsSemiBold: require('../assets/fonts/Poppins-SemiBold.ttf'),
+    poppinsBold: require('../assets/fonts/Poppins-Bold.ttf'),
+    poppinsExtraBold: require('../assets/fonts/Poppins-ExtraBold.ttf'),
+    poppinsLight: require('../assets/fonts/Poppins-Light.ttf'),
+    poppinsMedium: require('../assets/fonts/Poppins-Medium.ttf'),
+    poppinsRegular: require('../assets/fonts/Poppins-Regular.ttf'),
+    poppinsThin: require('../assets/fonts/Poppins-Thin.ttf'),
   });
 
   if (!loaded) {
@@ -18,12 +36,16 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <KeyboardProvider>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+          <StatusBar style="auto" />
+        </ThemeProvider>
+      </KeyboardProvider>
+    </QueryClientProvider>
   );
 }
